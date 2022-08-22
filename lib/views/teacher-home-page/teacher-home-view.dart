5d9dbb80edaead433b72d16e3dc26491acd46c3c
Ahmed <ad.ahmad.as123@gmail.com>
2022-08-22 17:07:45 +0300

    add background

--- lib/views/teacher-home-page/teacher-home-view.dart
+++ lib/views/teacher-home-page/teacher-home-view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kuttab/views/teacher-home-page/widgets.dart';
 
 class TeacherHomepage extends StatefulWidget {
   const TeacherHomepage({Key? key}) : super(key: key);
@@ -10,6 +11,32 @@ class TeacherHomepage extends StatefulWidget {
 class _TeacherHomepageState extends State<TeacherHomepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 200,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF4F946B),
+              Color(0xFF4F946B),
+            ],
+          ),
+          image: DecorationImage(
+            image: AssetImage('assets/images/decoration-gradiant.png'),
+            fit: BoxFit.cover,
+            alignment: Alignment.topCenter,
+            opacity: 1,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
