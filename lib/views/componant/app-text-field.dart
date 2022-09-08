24669b63a0a5040efedb59b5bb7989663e929508
Ahmed <ad.ahmad.as123@gmail.com>
2022-09-08 17:01:29 +0300

    update dimensions and add initialValue

--- lib/views/componant/app-text-field.dart
+++ lib/views/componant/app-text-field.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:kuttab/Utils/constants.dart';
 
 class AppTextField extends StatefulWidget {
+  String? initialValue = "";
   AppTextField({
     Key? key,
     required this.hintText,
+    this.initialValue,
     required this.obscure,
   }) : super(key: key);
 
@@ -19,6 +21,7 @@ class _AppTextFieldState extends State<AppTextField> {
   Widget build(BuildContext context) {
     return TextFormField(
       style: const TextStyle(
+        fontSize: 12,
         fontFamily: 'URW-DIN-Arabic',
       ),
       keyboardType: TextInputType.multiline,
@@ -26,6 +29,8 @@ class _AppTextFieldState extends State<AppTextField> {
       enableSuggestions: !widget.obscure,
       autocorrect: !widget.obscure,
       cursorColor: SECONDARY_COLOR,
+      initialValue:
+          (widget.initialValue == null) ? "" : "${widget.initialValue}",
       decoration: InputDecoration(
         suffixIcon: (!widget.obscure)
             ? (null)
@@ -44,18 +49,18 @@ class _AppTextFieldState extends State<AppTextField> {
                 },
               ),
         hintText: widget.hintText,
-        hintStyle: TextStyle(fontSize: 14),
+        hintStyle: TextStyle(fontSize: 12),
         filled: false,
         fillColor: SECONDARY_COLOR,
         contentPadding: const EdgeInsets.only(
-            left: 14.0, bottom: 6.0, top: 8.0, right: 14.0),
+            left: 24.0, bottom: 6.0, top: 8.0, right: 24.0),
         focusedBorder: OutlineInputBorder(
           borderSide: const BorderSide(color: PRIMARY_COLOR),
-          borderRadius: BorderRadius.circular(20.0),
+          borderRadius: BorderRadius.circular(16.0),
         ),
         enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: PRIMARY_COLOR),
-          borderRadius: BorderRadius.circular(20.0),
+          borderSide: const BorderSide(color: Colors.grey),
+          borderRadius: BorderRadius.circular(16.0),
         ),
       ),
     );
