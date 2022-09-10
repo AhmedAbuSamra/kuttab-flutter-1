3c23a1cb79d2b4a657be2fab85e2faf00fa17377
Ahmed <ad.ahmad.as123@gmail.com>
2022-09-10 21:44:55 +0300

    add initial componantes to page

---
+++ lib/views/settings-page/settings-view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:kuttab/views/componant/app-text.dart';
+import 'package:kuttab/views/componant/app-top-bar.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Color(0xFFf4f4f4),
+          image: DecorationImage(
+            image: AssetImage('assets/images/decoration-grey.png'),
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.topCenter,
+            opacity: 0.1,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: const [
+              SizedBox(height: 40),
+              AppTopBar(
+                title: 'الإعدادات',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
