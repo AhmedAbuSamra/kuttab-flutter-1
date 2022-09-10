ce15812cf021893a8c0288243001d417064742c5
Ahmed <ad.ahmad.as123@gmail.com>
2022-09-10 22:41:51 +0300

    add initial widgets to page

--- lib/views/about-page/about-view.dart
+++ lib/views/about-page/about-view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kuttab/views/componant/app-top-bar.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -10,6 +11,31 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Color(0xFFf4f4f4),
+          image: DecorationImage(
+            image: AssetImage('assets/images/decoration-grey.png'),
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.topCenter,
+            opacity: 0.1,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              const AppTopBar(
+                title: 'الإعدادات',
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
