f785ce0da85e50d3fcc734700d1b2bbcd0ad7572
Ahmed <ad.ahmad.as123@gmail.com>
2022-10-05 20:28:43 +0300

    create file add StudentAttendancebody

---
+++ lib/views/teacher-app/student-attendance-page/widgets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentAttendanceBody extends StatefulWidget {
+  const StudentAttendanceBody({Key? key}) : super(key: key);
+
+  @override
+  State<StudentAttendanceBody> createState() => _StudentAttendanceBodyState();
+}
+
+class _StudentAttendanceBodyState extends State<StudentAttendanceBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
